e88c2038622a4a5a74a18cced66101c6dc63e9f6
Will Figueiredo <will@wfigueiredo.com>
2019-03-29 00:53:49 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
 // Write your code in this file!
-scuberGreetingForFeet(199) {
-
+scuberGreetingForFeet(feet) {
+  if (feet <= 400) {
+    return 'This one is on me!'
+  } else if (1999 < feet && feet < 2500) {
+    return 'I will gladly take your thirty bucks.'
+  } else {
+    return "No can do."
+  }
 };
 
 ternaryCheckCity();
