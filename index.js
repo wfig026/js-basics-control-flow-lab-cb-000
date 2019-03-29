eb56b1591ad5d6fa1f35babfeb96dda416dc0903
Will Figueiredo <will@wfigueiredo.com>
2019-03-29 00:57:18 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 // Write your code in this file!
+function scuberGreetingForFeet (feet) {
+  if (feet <= 400) {
+    return 'This one is on me!';
+  } else if (1999 < feet && feet < 2500) {
+    return "I will gladly take your thirty bucks.";
+  } else {
+    return "No can do.";
+  }
+}
+
+function ternaryCheckCity(city) {
+  return city === 'NYC' ? 'Ok, sounds good.' : 'No go.'
+};
+
+function switchOnCharmFromTip(trip) {
+  switch(trip) {
+    case 'generous':
+    return 'Thank you so much.';
+    break
+    case 'not as generous':
+    return 'Thank you.';
+    break
+    case 'thanks for everything':
+    return 'Bye.';
+    break
+  }
+};
